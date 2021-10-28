14e7c39043d211435e486f4d1450233bca670bbe
Gabriel Koch Fodi <gabrielkochf@gmail.com>
2021-10-28 17:18:04 -0300

    creating modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header(){
+
+type HeaderProps = {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
+    
+
+ 
     return (
         <Container>
             <Content>
             <img src={logoImg} alt="dt monay" />
-            <button type='button'>
+            <button type='button' onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
+           
             </Content>
         </Container>
     )
